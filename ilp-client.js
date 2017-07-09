3cd9b652abdffeafc8fd427d2f1c01956719577b
Evan Schwartz <evan.mark.schwartz@gmail.com>
2017-07-09 12:09:52 +0200

    feat: use base64 token instead of url for sender creds

--- ilp-client.js
+++ ilp-client.js
@@ -1,9 +1,20 @@
 'use strict'
 
+const URL = require('url')
 const ILP = require('ilp')
 exports.SPSP = ILP.SPSP
 exports.IPR = ILP.IPR
 exports.PSK = ILP.PSK
 exports.Plugin = require('ilp-plugin-payment-channel-framework')
-exports.URL = require('url')
 
+exports.parseAuthToken = function (string) {
+  if (string.indexOf('ilp_secret:') === 0) {
+    string = string.replace('ilp_secret:', '')
+  }
+  const uri = Buffer.from(string, 'base64').toString('utf8')
+  let parsed = URL.parse(uri)
+  const auth = parsed.auth.split(':')
+  parsed.prefix = auth.username
+  parsed.token = auth.password
+  return parsed
+}
