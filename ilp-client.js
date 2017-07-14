07dc62b20e194915ce0e632fc2475e7ed1cf2367
Evan Schwartz <evan.mark.schwartz@gmail.com>
2017-07-14 15:03:11 +0200

    chore: use ilp-secret module

--- ilp-client.js
+++ ilp-client.js
@@ -6,15 +6,4 @@ exports.SPSP = ILP.SPSP
 exports.IPR = ILP.IPR
 exports.PSK = ILP.PSK
 exports.Plugin = require('ilp-plugin-payment-channel-framework')
-
-exports.parseAuthToken = function (string) {
-  if (string.indexOf('ilp_secret:') === 0) {
-    string = string.replace('ilp_secret:', '')
-  }
-  const uri = Buffer.from(string, 'base64').toString('utf8')
-  let parsed = URL.parse(uri)
-  const auth = parsed.auth.split(':')
-  parsed.prefix = auth[0]
-  parsed.token = auth[1]
-  return parsed
-}
+exports.Secret = require('ilp-secret')
